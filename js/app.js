763caa8f3296e911a265f95439cb2399d2af949d
Paulo-101 <phenrique4570@gmail.com>
2022-07-29 16:51:35 -0300

    alterado para formulario

--- js/app.js
+++ js/app.js
@@ -1,9 +1,11 @@
 function pegaDados() {
     //Recupera as informações do campo com id nome
     let nome = document.getElementById('nome').value;
-    let cpf = document.getElementById('cpf').value;
-    let contato = doccument.getElementById('contato').value;
+    let cpf = document.getElementById('email').value;
+    let contato = doccument.getElementById('mensagem').value;
 
     alert('Cadastro realizado com sucesso, em breve entraremos em contato com você!')
     
 }
+
+pegaDados();
